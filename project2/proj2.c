7c76fee9e5fa6e07487c4c5cbd7e0da39e272ac1
petak5 <peterurgos12345@gmail.com>
2020-04-24 15:02:48 +0200

    Just saving the little progress I have.

--- project2/proj2.c
+++ project2/proj2.c
@@ -1,19 +1,80 @@
 #include "proj2.h"
 
+FILE *f;
+semaphores_t sem;
+sm_t *sm;
+
 int main(int argc, char *argv[])
 {
     params_t par = parse_params(argc, argv);
 
-    FILE *f = fopen(FILE_PATH, "w");
-    if (f == NULL)
+    init_resources();
+
+    // Judge
+    pid_t judge_pid = fork();
+    if (judge_pid == 0)
     {
-        fprintf(stderr, FAILED_TOP_OPEN_FILE);
+        judge(par);
+    }
+    else if (judge_pid < 0)
+    {
+        fprintf(stderr, "Error: failed to fork this process.\n");
         return 1;
     }
 
-    fclose(f);
+    // Immigrants generator
+    pid_t imm_gen_pid = fork();
+    if (imm_gen_pid == 0)
+    {
+        imm_gen(par);
+    }
+    else if (imm_gen_pid < 0)
+    {
+        fprintf(stderr, "Error: failed to fork this process.\n");
+        return 1;
+    }
+
+    waitpid(judge_pid, NULL, 0);
+    waitpid(imm_gen_pid, NULL, 0);
+
+    sem_wait(sem.write);
+    fprintf(f, "Main process finishing...\n");
+    sem_post(sem.write);
+
+    free_resources();
 
     return 0;
+}
+
+void judge(params_t par)
+{
+    usleep((random() % (par.JG)) * 1000);
+
+    sem_wait(sem.write);
+    fprintf(f, "%d  JUDGE   wants to enter.\n", sm->message_count);
+    sm->message_count++;
+    sem_post(sem.write);
+
+    exit(0);
+}
+
+void imm_gen(params_t par)
+{
+    sem_wait(sem.write);
+    fprintf(f, "%d  Generating immigrants.\n", sm->message_count);
+    sm->message_count++;
+    sem_post(sem.write);
+
+    if (par.PI == 0) exit(0);
+
+    exit(0);
+}
+
+void immigrant(params_t par)
+{
+    if (par.PI == 0) exit(0);
+
+    exit(0);
 }
 
 params_t parse_params(int argc, char **argv)
@@ -40,8 +101,8 @@ params_t parse_params(int argc, char **argv)
     s = NULL;
     temp = strtol(argv[2], &s, 10);
     if ((strcmp(s, "") != 0) ||
-        (par.IG < 0) ||
-        (par.IG > 2000))
+        (temp < 0) ||
+        (temp > 2000))
     {
         fprintf(stderr, INVALID_ARGUMENTS_MESSAGE);
         exit(1);
@@ -51,8 +112,8 @@ params_t parse_params(int argc, char **argv)
     s = NULL;
     temp = strtol(argv[3], &s, 10);
     if ((strcmp(s, "") != 0) ||
-        (par.JG < 0) ||
-        (par.JG > 2000))
+        (temp < 0) ||
+        (temp > 2000))
     {
         fprintf(stderr, INVALID_ARGUMENTS_MESSAGE);
         exit(1);
@@ -62,8 +123,8 @@ params_t parse_params(int argc, char **argv)
     s = NULL;
     temp = strtol(argv[4], &s, 10);
     if ((strcmp(s, "") != 0) ||
-        (par.IT < 0) ||
-        (par.IT > 2000))
+        (temp < 0) ||
+        (temp > 2000))
     {
         fprintf(stderr, INVALID_ARGUMENTS_MESSAGE);
         exit(1);
@@ -73,8 +134,8 @@ params_t parse_params(int argc, char **argv)
     s = NULL;
     temp = strtol(argv[5], &s, 10);
     if ((strcmp(s, "") != 0) ||
-        (par.JT < 0) ||
-        (par.JT > 2000))
+        (temp < 0) ||
+        (temp > 2000))
     {
         fprintf(stderr, INVALID_ARGUMENTS_MESSAGE);
         exit(1);
@@ -82,4 +143,44 @@ params_t parse_params(int argc, char **argv)
     par.JT = temp;
 
     return par;
+}
+
+void init_resources()
+{
+    // Output file
+    f = fopen(FILE_PATH, "w");
+    if (f == NULL)
+    {
+        fprintf(stderr, FAILED_TO_OPEN_FILE);
+        exit(1);
+    }
+
+    // Semaphores
+    /* is this needed? */sem_unlink(SEM_NAME("write"));
+    if ((sem.write = sem_open(SEM_NAME("write"), O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
+    {
+        fprintf(stderr, "Error: failed to open semaphore.\n");
+        exit(1);
+    }
+
+    // Shared memory
+    if ((sm = mmap(NULL, sizeof(sm_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0)) == MAP_FAILED)
+    {
+        fprintf(stderr, "Error: failed to allocate shared memory.\n");
+        exit(1);
+    }
+    sm->message_count = 1;
+}
+
+void free_resources()
+{
+    // Output file
+    fclose(f);
+
+    // Semaphores
+    sem_unlink(SEM_NAME("write"));
+    sem.write = NULL;
+
+    // Shared Memory
+    munmap(sm, sizeof(sm_t));
 }
--- project2/proj2.h
+++ project2/proj2.h
@@ -1,14 +1,22 @@
+#ifndef __PROJ2_H__
+#define __PROJ2_H__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
 #include <semaphore.h>
 
 #define INVALID_ARGUMENTS_MESSAGE "Error: invalid arguments.\n"
-#define FAILED_TOP_OPEN_FILE "Error: failed to open file.\n"
+#define FAILED_TO_OPEN_FILE "Error: failed to open file.\n"
+
+#define SEM_NAME(n) "xurgos00.ios.proj2."#n
 
 #define FILE_PATH "proj2.out"
 
-typedef struct params {
+typedef struct params
+{
     long PI;
     int IG;
     int JG;
@@ -16,4 +24,27 @@ typedef struct params {
     int JT;
 } params_t;
 
+typedef struct semaphores
+{
+    sem_t *write;
+} semaphores_t;
+
+typedef struct sm
+{
+    int message_count;
+    int entered;
+} sm_t;
+
 params_t parse_params(int argc, char **argv);
+
+void init_resources();
+
+void free_resources();
+
+void judge(params_t par);
+
+void imm_gen(params_t par);
+
+void immigrant(params_t par);
+
+#endif // __PROJ2_H__
