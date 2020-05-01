0a335a3232601f644c69de20f86819ecc90f96d5
petak5 <peterurgos12345@gmail.com>
2020-05-01 12:44:48 +0200

    Some more neccessary imports.

--- project2/proj2.h
+++ project2/proj2.h
@@ -8,6 +8,8 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <semaphore.h>
 
 #define INVALID_ARGUMENTS_MESSAGE "Error: invalid arguments.\n"
